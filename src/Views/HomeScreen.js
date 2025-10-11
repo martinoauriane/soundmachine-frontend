eacdc3ae5b8df8e695aa348f318a1f45c007997b
movauriane <auriane.martino@movu-robotics.com>
2025-10-11 08:42:44 +0200

    added sound

--- src/Views/HomeScreen.js
+++ src/Views/HomeScreen.js
@@ -5,13 +5,28 @@ import SearchBar from "../components/InputSearch";
 import UploadSound from "../components/UploadSound";
 import MostPopularSoundsComponent from "../components/MostPopularSoundsComponent";
 import BrowseByCategories from "../components/BrowseByCategoriesComponent";
-import AudioPlayer from "../components/AudioPlayer";
+import TrackPlayer from "../components/TrackPlayer";
 
 //style
 import "../css/navbar.css";
 import "../css/widgets.css";
 
 function HomeScreen() {
+  let soundsArray = [
+    "/sounds/merry-christmas-sounds.mp3",
+    "/sounds/emotional-ambient.mp3",
+    "/sounds/jungle-waves.mp3",
+  ];
+
+  function soundName(soundUrl) {}
+
+  const listSounds = soundsArray.map((soundUrl) => {
+    return (
+      <div className="rendering-track">
+        <TrackPlayer trackUrl={soundUrl} />
+      </div>
+    );
+  });
   return (
     <div className="main">
       <div className="nav-bar">
@@ -33,7 +48,7 @@ function HomeScreen() {
         <MostPopularSoundsComponent />
         <UploadSound />
       </div>
-      <AudioPlayer />
+      <div>{listSounds}</div>
     </div>
   );
 }
